4574faba477200d72e7e40d284e06be0f74374be
ben888 <br@massa.net>
2025-11-07 18:50:23 +0700

    fix url to use ngrok

--- src/config/protocol.ts
+++ src/config/protocol.ts
@@ -12,7 +12,7 @@ export interface ProtocolConfig {
 }
 
 export const protocolConfig: ProtocolConfig = {
-  baseUrl: ' http://145.239.66.206:3001/api', // Relative URL → Vite proxy intercepte en dev
+  baseUrl: 'https://yearningly-environmental-jamila.ngrok-free.dev//api', // Relative URL → Vite proxy intercepte en dev
   timeout: 10000, // 10 seconds
   retryAttempts: 3,
 };
