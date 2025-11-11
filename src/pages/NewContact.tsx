c99a9786625639eb28ee9bf283e1d386da1d56a3
ben888 <br@massa.net>
2025-11-11 19:54:35 +0700

    Update file selection label in NewContact component for clarity

--- src/pages/NewContact.tsx
+++ src/pages/NewContact.tsx
@@ -277,7 +277,7 @@ const NewContact: React.FC = () => {
                           d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
                         />
                       </svg>
-                      <span>Choose .yaml file</span>
+                      <span>Choose a file</span>
                     </>
                   )}
                 </label>
